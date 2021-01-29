676e3c39b96951cc61bfe5e00d486b14f05e8dff
Zenoscitius <8884084+Zenoscitius@users.noreply.github.com>
2021-01-28 18:11:33 -0600

    Health and damage zones
    Started into enemy implementation

--- Project S - Main/Assets/Scripts/LeviController.cs
+++ Project S - Main/Assets/Scripts/LeviController.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class LeviController : MonoBehaviour
 {
+    public float speed = 3.0f;
+
     public int maxHealth = 5;
+    public float timeInvincible = 2.0f;
+
+    public int health { get { return currentHealth; } }
     int currentHealth;
+
+    bool isInvincible;
+    float invincibleTimer;
 
     Rigidbody2D rigidbody2d;
     float horizontal;
@@ -15,7 +23,6 @@ public class LeviController : MonoBehaviour
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
-
         currentHealth = maxHealth;
     }
 
@@ -24,21 +31,36 @@ public class LeviController : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+                isInvincible = false;
+        }
     }
 
     void FixedUpdate()
     {
         Vector2 position = rigidbody2d.position;
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
 
         rigidbody2d.MovePosition(position);
     }
 
-    void ChangeHealth(int amount)
+    public void ChangeHealth(int amount)
     {
+        if (amount < 0)
+        {
+            if (isInvincible)
+                return;
+
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
+
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
-
 }
